3b582ccde6a21aff5e52eeefc38eb0d33ad8622b
Deceit-87 <manuelfreund318@gmail.com>
2022-03-05 19:57:35 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+
+      email:[],
+
+    },
+
+    created() {
+        for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                 .then( (eMail) => {
+                  console.log(eMail.data.response);
+                  eMail = eMail.data.response
+                  this.email.push(eMail);
+                })
+            }
+                
+
+             console.log(this.email)
+   },
+
+}) 
